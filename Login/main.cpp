d9a969ebe46d32fb5c4bbccb5d07ef3bb52d8a08
Eryn M <johnmary1010@gmail.com>
2018-03-31 08:22:52 -0500

    Connected database in main.cpp

--- Login/main.cpp
+++ Login/main.cpp
@@ -1,5 +1,9 @@
 #include "loginpage.h"
 #include <QApplication>
+#include <QSqlQuery>
+#include <QSqlDatabase>
+#include <QDebug>
+#include <QSqlError>
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +11,41 @@ int main(int argc, char *argv[])
     LoginPage w;
     w.show();
 
-    return a.exec();
-}
+    //Set var
+            QSqlDatabase db = QSqlDatabase::addDatabase("QODBC3");
+
+            //Set connection string
+            db.setDatabaseName("DRIVER={SQL Server};Server=localhost\\SQLEXPRESS01;Database=GbManuf;Trusted_Connection=True");
+            //connecting to database
+             db.open();
+             if(db.open())
+             {
+                qDebug() << "Database Connected!";
+
+                //Test qry the database
+                QSqlQuery qry;
+
+                if(qry.exec("SELECT * FROM [GbManuf].[dbo].[Employee]"))
+                {
+                    while(qry.next())
+                    {
+                        qDebug() << qry.value(1).toString();
+                    }
+                }
+                else
+                {
+                    qDebug() << "Database Closed!";
+                }
+
+                db.close();
+             }
+             else
+             {
+                 qDebug() << "Failed to connect = " << db.lastError().text();
+             }
+            // Source string
+            //Server=localhost\SQLEXPRESS01;Database=master;Trusted_Connection=True;
+
+
+        return a.exec();
+    }
